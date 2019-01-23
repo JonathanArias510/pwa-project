ae93428ed0664d725e7bf9e7cbe2605ad897b0c4
Jonathan Arias <andresarias510@gmail.com>
2019-01-23 11:37:50 -0500

    ios friendly

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v3';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 
@@ -49,6 +49,10 @@ self.addEventListener('activate', event => {
 
     keys.forEach(cacheKey => {
       if (cacheKey !== STATIC_CACHE && cacheKey.includes('static')) {
+        return caches.delete(cacheKey);
+      }
+
+      if (cacheKey !== DYNAMIC_CACHE && cacheKey.includes('dynamic')) {
         return caches.delete(cacheKey);
       }
     })
