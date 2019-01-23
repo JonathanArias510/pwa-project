9d6a1ad4a07b3722b4d6c27073910804273547c3
Jonathan Arias <andresarias510@gmail.com>
2019-01-23 09:20:56 -0500

    Fix urls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('/js/sw-utils.js');
+importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
@@ -6,16 +6,16 @@ const INMUTABLE_CACHE = 'inmutable-v1'
 
 
 const APP_SHELL_STATIC = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/img/favicon.ico',
-  '/img/avatars/hulk.jpg',
-  '/img/avatars/ironman.jpg',
-  '/img/avatars/spiderman.jpg',
-  '/img/avatars/thor.jpg',
-  '/img/avatars/wolverine.jpg',
-  '/js/app.js'
+  //'/',
+  'index.html',
+  'css/style.css',
+  'img/favicon.ico',
+  'img/avatars/hulk.jpg',
+  'img/avatars/ironman.jpg',
+  'img/avatars/spiderman.jpg',
+  'img/avatars/thor.jpg',
+  'img/avatars/wolverine.jpg',
+  'js/app.js'
 
 ];
 
@@ -23,8 +23,8 @@ const APP_SELL_INMUTABLE = [
   'https://fonts.googleapis.com/css?family=Quicksand:300,400',
   'https://fonts.googleapis.com/css?family=Lato:400,300',
   'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-  '/css/animate.css',
-  '/js/libs/jquery.js'
+  'css/animate.css',
+  'js/libs/jquery.js'
 ]
 
 
